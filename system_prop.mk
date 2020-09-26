715ebd35eb2a70031a3408d3d909fedb9663bfa9
devil-black-786 <fahimahmad904@gmail.com>
2020-09-26 09:08:20 +0000

    CPH1859: Adding Zygote prefoking and custom tweaks

--- system_prop.mk
+++ system_prop.mk
@@ -186,3 +186,18 @@ debug.composition.type=gpu
 #Enable Surface Flinger
 PRODUCT_PROPERTY_OVERRIDES += \ 
 debug.sf.latch_unsignaled=1
+
+# Zygote preforking
+persist.device_config.runtime_native.usap_pool_enabled=true
+
+# Properties
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.config.fha_enable=true \
+    ro.sys.fw.bg_apps_limit=32 \
+    ro.config.dha_cached_max=16 \
+    ro.config.dha_empty_max=42 \
+    ro.config.dha_empty_init=32 \
+    ro.config.dha_lmk_scale=0.545 \
+    ro.config.dha_th_rate=2.3 \
+    ro.config.sdha_apps_bg_max=64 \
+    ro.config.sdha_apps_bg_min=8
