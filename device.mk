f3f72855eb750c0f64b99c0cd3ac940168946132
Maanush Putcha <p.maanush@gmail.com>
2020-11-18 19:49:01 +0530

    CPH1859: Move privapp permissions to device tree.

--- device.mk
+++ device.mk
@@ -49,6 +49,8 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.secure_lock_screen.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/android.software.secure_lock_screen.xml \
     frameworks/native/data/etc/handheld_core_hardware.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/handheld_core_hardware.xml \
     $(LOCAL_PATH)/permissions/privapp-permissions-oprecorder.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/privapp-permissions-oprecorder.xml 
+    $(LOCAL_PATH)/permissions/com.mediatek.op.ims.common.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/com.mediatek.op.ims.common.xml \
+    $(LOCAL_PATH)/permissions/privapp-permissions-mediatek.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/privapp-permissions-mediatek.xml
 
 # OPScreenRecorder
  PRODUCT_PACKAGES += \
